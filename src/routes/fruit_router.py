ab0d1a0ebbcd7b5d587f40bd2c2bdbb843224643
Gabriel Cavalcante <gabriel.cavalcant.pb@compasso.com.br>
2024-12-15 13:18:35 -0300

    feat: model para fruit

--- src/routes/fruit_router.py
+++ src/routes/fruit_router.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 from controllers.fruit_controller import FruitController
+from models.fruit_model import FruitResponse
 
 fruit_router = APIRouter()
 
-@fruit_router.get('/')
+@fruit_router.get('/', response_model=FruitResponse)
 async def get_random_fruit():
-    fruits = await FruitController.get_random_fruit()
-    return fruits
+    return await FruitController.get_random_fruit()
