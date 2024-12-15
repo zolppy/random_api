07df07d2a230de03d21ebb7b712c99595dd8d493
Gabriel Cavalcante <gabriel.cavalcant.pb@compasso.com.br>
2024-12-15 12:43:57 -0300

    test: teste para rota country

---
+++ src/tests/test_route_country.py
@@ -0,0 +1,12 @@
+import pytest
+from httpx import AsyncClient
+from utils.text import load_from_txt
+
+@pytest.mark.asyncio
+async def test_get_random_country():
+    async with AsyncClient(base_url="http://localhost:8000/random/country") as client:
+        res = await client.get("/")
+    countries = load_from_txt('src/data/countries.txt')
+
+    assert res.status_code == 200
+    assert res.json() in countries
