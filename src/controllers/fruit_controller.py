6fcdc5e9285df7cccada7edd3b6c8d16f8cfd309
Gabriel Cavalcante <gabriel.cavalcant.pb@compasso.com.br>
2024-12-15 13:17:11 -0300

    fix: mudança nas respostas dos controladores

--- src/controllers/fruit_controller.py
+++ src/controllers/fruit_controller.py
@@ -4,8 +4,8 @@ from utils.text import load_from_txt
 
 class FruitController:
     @staticmethod
-    async def get_random_fruit() -> str:
+    async def get_random_fruit() -> dict[str, str]:
         base_dir = Path(__file__).resolve().parent.parent
         file_path = base_dir / "data" / "fruits.txt"
         fruits = load_from_txt(str(file_path))
-        return fruits[randrange(len(fruits))]
+        return {"fruit_name": {fruits[randrange(len(fruits))]}}
